a4a0a4839bb6bc2c473f120ab33660997e84527a
Андрей Кумыков <aakumykov@yandex.ru>
2020-06-06 11:26:39 +0700

    SingleLineRenderer.java

---
+++ lib/src/main/java/com/github/aakumykov/lib/SingleLineRenderer.java
@@ -0,0 +1,47 @@
+package com.github.aakumykov.lib;
+
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.text.Layout;
+
+class SingleLineRenderer extends TextRoundedBgRenderer {
+
+    private int horizontalPadding;
+    private int verticalPadding;
+    private Drawable drawable;
+
+    /**
+     * Base class for single and multi line rounded background renderers.
+     *
+     * @param horizontalPadding the padding to be applied to left & right of the background
+     * @param verticalPadding   the padding to be applied to top & bottom of the background
+     */
+    public SingleLineRenderer(int horizontalPadding, int verticalPadding, int horizontalPadding1, int verticalPadding1, Drawable drawable) {
+        super(horizontalPadding, verticalPadding);
+        this.horizontalPadding = horizontalPadding1;
+        this.verticalPadding = verticalPadding1;
+        this.drawable = drawable;
+    }
+
+
+    @Override
+    public void draw(
+            Canvas canvas,
+            Layout layout,
+            int startLine,
+            int endLine,
+            int startOffset,
+            int endOffset
+    ) {
+        int lineTop = getLineTop(layout, startLine);
+        int lineBottom = getLineBottom(layout, startLine);
+
+        // get min of start/end for left, and max of start/end for right since we don't
+        // the language direction
+        int left = Math.min(startOffset, endOffset);
+        int right = Math.max(startOffset, endOffset);
+
+        drawable.setBounds(left, lineTop, right, lineBottom);
+        drawable.draw(canvas);
+    }
+}
