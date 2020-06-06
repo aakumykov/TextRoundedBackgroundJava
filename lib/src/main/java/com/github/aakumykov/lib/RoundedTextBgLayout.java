d3ceeb43436caf127724e2b8a72a5b7851da475d
Андрей Кумыков <aakumykov@yandex.ru>
2020-06-06 10:39:16 +0700

    RoundedTextBgLayout.getLineBottomWithoutSpacing()

---
+++ lib/src/main/java/com/github/aakumykov/lib/RoundedTextBgLayout.java
@@ -0,0 +1,64 @@
+package com.github.aakumykov.lib;
+
+import android.os.Build;
+import android.text.Layout;
+import android.text.TextPaint;
+
+public abstract class RoundedTextBgLayout extends Layout {
+
+    private final static float DEFAULT_LINESPACING_EXTRA = 0f;
+    private final static float DEFAULT_LINESPACING_MULTIPLIER = 1f;
+
+
+    /**
+     * Subclasses of Layout use this constructor to set the display text,
+     * width, and other standard properties.
+     *
+     * @param text        the text to render
+     * @param paint       the default paint for the layout.  Styles can override
+     *                    various attributes of the paint.
+     * @param width       the wrapping width for the text.
+     * @param align       whether to left, right, or center the text.  Styles can
+     *                    override the alignment.
+     * @param spacingMult factor by which to scale the font size to get the
+     *                    default line spacing
+     * @param spacingAdd  amount to add to the default line spacing
+     */
+    protected RoundedTextBgLayout(CharSequence text, TextPaint paint, int width, Alignment align, float spacingMult, float spacingAdd) {
+        super(text, paint, width, align, spacingMult, spacingAdd);
+    }
+
+    public int getLineBottomWithoutSpacing(int line) {
+        int lineBottom = getLineBottom(line);
+        boolean lastLineSpacingNotAdded = Build.VERSION.SDK_INT >= 19;
+        boolean isLastLine = line == (getLineCount() - 1);
+
+        int lineBottomWithoutSpacing;
+        float lineSpacingExtra = getSpacingAdd();
+        float lineSpacingMultiplier = getSpacingMultiplier();
+        boolean hasLineSpacing = lineSpacingExtra != DEFAULT_LINESPACING_EXTRA
+                || lineSpacingMultiplier != DEFAULT_LINESPACING_MULTIPLIER;
+
+        if (!hasLineSpacing || isLastLine && lastLineSpacingNotAdded) {
+            lineBottomWithoutSpacing = lineBottom;
+        }
+        else {
+            float extra;
+
+            if (0 != Float.compare(lineSpacingMultiplier, DEFAULT_LINESPACING_MULTIPLIER)) {
+                int lineHeight = getLineHeight(line);
+                extra = lineHeight - (lineHeight - lineSpacingExtra) / lineSpacingMultiplier;
+            } else {
+                extra = lineSpacingExtra;
+            }
+
+            lineBottomWithoutSpacing = (int) (lineBottom - extra);
+        }
+
+        return lineBottomWithoutSpacing;
+    }
+
+    public int getLineHeight(int line) {
+
+    }
+}
